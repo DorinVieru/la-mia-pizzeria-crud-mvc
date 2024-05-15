a0e2781cf90dcdd10d7afc690f59169fbd9097e4
DorinVieru <dorino.vieru@gmail.com>
2024-05-15 16:33:33 +0200

    UP: Write PizzaController

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace la_mia_pizzeria_static.Controllers
 {
     public class PizzaController : Controller
     {
+        private readonly PizzaContext _context;
+
+        public PizzaController(PizzaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var pizze = _context.Pizze.ToList();
+            return View(pizze);
         }
+
+        
     }
 }
