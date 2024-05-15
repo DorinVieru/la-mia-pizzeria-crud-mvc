1ef65a2c2f1fa2e42759b5019a9f06b935611a07
DorinVieru <dorino.vieru@gmail.com>
2024-05-15 14:15:07 +0200

    UP: delete Snacks folder

--- Snacks/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Snacks.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
