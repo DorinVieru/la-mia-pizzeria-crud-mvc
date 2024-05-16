2c95bf4128fb3cbed09c6068f9c515c495bd92ed
DorinVieru <dorino.vieru@gmail.com>
2024-05-16 14:16:49 +0200

    UP: Add validation for all input of form

--- la-mia-pizzeria-static/Models/Pizze.cs
+++ la-mia-pizzeria-static/Models/Pizze.cs
@@ -5,9 +5,21 @@ namespace la_mia_pizzeria_static.Models
     public class Pizze
     {
         [ Key ] public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Il nome della pizza può avere un massimo di 50 caratteri (nessun nome è così lungo!).")]
+        [Required(ErrorMessage = "Il nome della pizza è obbligatorio.")]
         public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Il nome della pizza può avere un massimo di 500 caratteri.")]
+        [Required(ErrorMessage = "La descrizione della pizza è obbligatoria.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "L'immagine della pizza è obbligatoria.")]
         public string Img { get; set; }
+
+        [Required(ErrorMessage = "Il prezzo della pizza è obbligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Il prezzo della pizza deve essere superiore a 0,00€.")]
+        [RegularExpression(@"^\d+\.\d{0,2}$", ErrorMessage = "Il prezzo della pizza deve essere in formato numerico con massimo due decimali.")]
         public double Price { get; set; }
 
         public Pizze() { }
