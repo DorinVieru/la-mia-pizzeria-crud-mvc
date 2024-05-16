c24be2f466eedfc827cb961c80a779e50c17ebc4
DorinVieru <dorino.vieru@gmail.com>
2024-05-16 13:08:33 +0200

    UP: Add action get and post on PizzaController

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -1,5 +1,6 @@
 ﻿using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -19,6 +20,27 @@ namespace la_mia_pizzeria_static.Controllers
                 return NotFound();
             }
             return View(pizza);
+        }
+
+        // Action GET che fornisce la view della FORM
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Chiamata POST che avviene tramite il form passandogli i dati della pizza inserita
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create (Pizze pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", pizza); // Ritorna alla view in cui è presente il form
+            }
+
+            PizzaManager.InsertPizza(pizza.Name, pizza.Description, pizza.Img, pizza.Price);
+            return RedirectToAction("Index");
         }
     }
 }
