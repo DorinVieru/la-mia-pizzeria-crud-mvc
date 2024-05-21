f730fb0a15e442bbbbf78262a58b9566a9bf142f
DorinVieru <dorino.vieru@gmail.com>
2024-05-21 15:24:28 +0200

    UP: Modify PizzaFormModel to add Ingredients

--- la-mia-pizzeria-static/Models/PizzaFormModel.cs
+++ la-mia-pizzeria-static/Models/PizzaFormModel.cs
@@ -1,9 +1,13 @@
-﻿namespace la_mia_pizzeria_static.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace la_mia_pizzeria_static.Models
 {
     public class PizzaFormModel
     {
         public Pizze Pizza { get; set; }
         public List<Category>? Categories { get; set; }
+        public List<SelectListItem>? Ingredients { get; set; } // Gli ingredienti selezionabili 
+        public List<string>? SelectedIngredients { get; set; } // Gli ID degli ingredienti effettivamente selezionati
 
         public PizzaFormModel() { }
     }
